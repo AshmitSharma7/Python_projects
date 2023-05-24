cf38436062132fff4ef40e1fa61259e6cb2ff37b
Ashmit Sharma <97040469+AshmitSharma7@users.noreply.github.com>
2023-05-24 16:16:38 +0530

    Update Sum_n_natural_no.py

--- Sum_n_natural_no.py
+++ Sum_n_natural_no.py
@@ -1,4 +1,5 @@
-num = int(input("Enter the number to which you want to find sum :"))
+# This program can find the sum of n natural numbers
+num = int(input("Enter the number :"))
 Sum = 0
 while num > 0:
     Sum += num
